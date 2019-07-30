2eb17f40502fd961175fc68c4a84bbf2d6f35aa8
pk1406 <pavan.g.kamath@gmail.com>
2019-07-30 15:15:32 +0530

    check if user already exists

--- go_auth/controller/controller.go
+++ go_auth/controller/controller.go
@@ -22,46 +22,60 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var user model.User
 
-	//decode body into user struct
-	json.NewDecoder(r.Body).Decode(&user)
-
-	//check if username already exists or not
-	// var tag string
-
-	// rs := db.QueryRow("SELECT username FROM users where username = ?", user.Username).Scan(tag)
-
-	// if tag != nil {
-	// 	json.NewEncoder(w).Encode("username ")
-	// }
-
-	//hash the password
-	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
+	db, err := sql.Open("mysql", "root:abcd@tcp(127.0.0.1:3306)/user")
 
 	if err != nil {
 		error := "Error while hashing"
 		json.NewEncoder(w).Encode(error)
 	}
 
-	user.Password = string(hash)
+	//decode body into user struct
+	json.NewDecoder(r.Body).Decode(&user)
 
-	//append to users slice.
-	users = append(users, user)
+	//check if username already exists or not
+	var tag string
+	var pass string
 
-	db, err := sql.Open("mysql", "root:abcd@tcp(127.0.0.1:3306)/user")
+	rs := db.QueryRow("SELECT username,password FROM users where username = ?", user.Username).Scan(&tag, &pass)
 
-	if err != nil {
-		panic(err.Error())
+	if string(tag) == string(user.Username) {
+		json.NewEncoder(w).Encode("user already exists")
+		defer db.Close()
+	} else {
+		fmt.Println(rs)
+
+		// if tag != nil {
+		// 	json.NewEncoder(w).Encode("username already exists")
+		// }
+
+		//hash the password
+		hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
+
+		if err != nil {
+			error := "Error while hashing"
+			json.NewEncoder(w).Encode(error)
+		}
+
+		user.Password = string(hash)
+
+		//append to users slice.
+		users = append(users, user)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		insert, err := db.Query("INSERT INTO users VALUES ( ?, ? )", user.Username, user.Password)
+
+		if err != nil {
+			error := err
+			json.NewEncoder(w).Encode(error)
+		}
+		fmt.Println(insert)
+		defer db.Close()
+		json.NewEncoder(w).Encode(user)
 	}
 
-	insert, err := db.Query("INSERT INTO users VALUES ( ?, ? )", user.Username, user.Password)
-
-	if err != nil {
-		error := err
-		json.NewEncoder(w).Encode(error)
-	}
-	fmt.Println(insert)
-	defer db.Close()
-	json.NewEncoder(w).Encode(user)
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
